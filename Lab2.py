ade66a6a3a881dd9edcbe7bc76bbab0661a0d969
yousufquraishi <yousufquraishi12@gmail.com>
2022-06-10 03:19:07 +0500

    hello world

--- Lab2.py
+++ Lab2.py
@@ -6,6 +6,10 @@ def print_command_line_args():
     print("1st Variable : ",sys.argv[1])
     print("2nd Variable : ",sys.argv[2])
     print("Script and variables : ",sys.argv[0],sys.argv[1],sys.argv[2])
-    
+
+
+def helloWorld():
+    print("Hello world")    
+
 if __name__ == "__main__":
     print_command_line_args()
